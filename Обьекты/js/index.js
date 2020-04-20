9db54754a1efa33c894b783d8a86a3d0a750df83
VladaGaydey <54859369+VladaGaydey@users.noreply.github.com>
2020-04-20 12:30:21 +0300

    Add files via upload

---
+++ Обьекты/js/index.js
@@ -0,0 +1,15 @@
+var  city1  =  {
+    Название : "Константинополь" ,
+    Население : 100000000 ,
+    GetName : function ( ) { console . log ( this . name ) } ,
+    exportStr : function ( ) { console . журнал ( это . имя , это . население ) }
+} ;
+
+var  city2  =  {
+    Название : "Еленендорф" ,
+    Население : 10 ^ 6 ,
+    GetName : function ( ) { console . log ( this . name ) } ,
+    exportStr : function ( ) { console . журнал ( это . имя , это . население ) }
+} ;
+
+город1 . test = "tdjp" ;
